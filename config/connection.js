7f270396b9c700c61f4841c650f55d81fc1f43e1
Peace Akpai <peaceawo@gmail.com>
2019-10-08 14:46:11 -0600

    connection 2

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var Sequelize = require("sequelize");
 require("dotenv").config();
 
 var mysql = require("mysql");
-
+var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -19,25 +19,15 @@ if (process.env.JAWSDB_URL) {
 // Make connection.
 connection.connect();
 
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// Export connection for our ORM to use.
 module.exports = connection;
 
 //Create Connection to database with Sequelize
 
-// var sequelize = new Sequelize("blogger", "root", process.env.DB_PASS, {
-//   host: "localhost",
-//   port: 3306,
-//   dialect: "mysql"
-// });
+var sequelize = new Sequelize("blogger", "root", process.env.DB_PASS, {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql"
+});
 
 //export object
 module.exports = sequelize;
